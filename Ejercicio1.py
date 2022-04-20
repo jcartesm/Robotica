bd58851d014b2a653490e37e5cc586472a8f10ca
NachoC <jcartes2019@alu.uct.cl>
2022-04-20 19:11:43 -0400

    cambios en funcion escnario y tambores para posisiones aleatorias al iniciar

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -28,10 +28,13 @@ def my_Escenario():
     BordeCorto_2 = box(pos=vector(-1000,0,0),size=vector(40,40,1000), color = color.gray(0.5))
     Bordelargo_1 = box(pos=vector(0,0,+500),size=vector(2000,40,40), color = color.gray(0.5))
     Bordelargo_2 = box(pos=vector(0,0,-500),size=vector(2000,40,40), color = color.gray(0.5))
-    D_1 = box(pos=vector(900,5,-400), size=vector(75,10,75), color = color.yellow)
-    D_2 = box(pos=vector(900,5,400), size=vector(75,10,75), color = color.yellow)
-    D_3 = box(pos=vector(-900,5,-400), size=vector(75,10,75), color = color.yellow)
-    D_4 = box(pos=vector(-900,5,400), size=vector(75,10,75), color = color.yellow)
+    D_1 = box(pos=vector(ra.randint(0,900),5,ra.randint(-400,0)), size=vector(75,10,75), color = color.yellow)
+    D_2 = box(pos=vector(ra.randint(0,900),5,ra.randint(0,400)), size=vector(75,10,75), color = color.yellow)
+    D_3 = box(pos=vector(ra.randint(-900,0),5,ra.randint(-400,0)), size=vector(75,10,75), color = color.yellow)
+    D_4 = box(pos=vector(ra.randint(-900,0),5,ra.randint(0,400)), size=vector(75,10,75), color = color.yellow)
+
+    # arreglo de posiciones de espacio para dejar tambor 
+    aD = [D_1.pos, D_2.pos, D_3.pos, D_4.pos]
     
     # Figura protocolar del prototipo P.E.N.E.
     # c1 = sphere(pos=vector(5,150,-500),radius=50, color = color.orange)
@@ -43,9 +46,12 @@ def my_Escenario():
     return
 
 # Creacion de tambores, x { -1000 ..... 1000} y x { -500 ..... 500}
-def my_Tambores(x1,x2,y1,y2):
-    posicion = RandomCoord(x1,x2,y1,y2)
-    Tambor_1  = cylinder( pos=vector(posicion[0],0,posicion[1]), axis=vector(0,60,0), radius=30, color=color.blue) 
+def my_Tambores():
+
+    Tambor_1  = cylinder( pos=vector(ra.randint(0,900),0,ra.randint(-400,0)), axis=vector(0,60,0), radius=30, color=color.blue) 
+    Tambor_2  = cylinder( pos=vector(ra.randint(0,900),0,ra.randint(0,400)), axis=vector(0,60,0), radius=30, color=color.blue)
+    Tambor_3  = cylinder( pos=vector(ra.randint(-900,0),0,ra.randint(-400,0)), axis=vector(0,60,0), radius=30, color=color.blue)
+    Tambor_4  = cylinder( pos=vector(ra.randint(-900,0),0,ra.randint(0,400)), axis=vector(0,60,0), radius=30, color=color.blue)  
 #---------------------------Definicion del Robot---------------------------------------#
 def my_Robot(nDesp):
     
@@ -111,7 +117,8 @@ def robotPrueba(x,y):
     BrazoL = box(pos=vector(x+52, 27,y-40), size=vector(100,10,8), color=color.blue)
     
     Brazos = compound([AnteBrazoL, AnteBrazoR, BrazoL, BrazoR])
-    robot1 = compound([Base, Lado1, Lado2, Lado3, Lado4, Rueda1, Rueda2, RuedaCastor])
+    robot1 = compound([Base, Lado1, Lado2, Lado3, Lado4, Rueda1, Rueda2, RuedaCastor, Brazos, centro])
+    robot2 = compound([Base, Lado1, Lado2, Lado3, Lado4, Rueda1, Rueda2, RuedaCastor, Brazos, centro])
 
     return  
 
@@ -122,7 +129,9 @@ nDesp=40
 centroide = [0,0]
 tPosInicial = (nDesp,0,0)
 
-my_Escenario() 
-my_Tambores(500,502,12,14)
-#my_Robot(nDesp)
-robotPrueba(centroide[0],centroide[1])
+posD = my_Escenario() 
+print(posD) 
+my_Tambores() 
+#my_Robot(nDesp) 
+robotPrueba(centroide[0],centroide[1]) 
+# print(aRP)
