08d7fef68601d90163793137696dcf9d19148cfa
Jorge Conejeros <jorgeconejerosrios@gmail.com>
2022-04-19 20:12:46 -0400

    Captacion de las coordenadas del click

--- Ventana2.py
+++ Ventana2.py
@@ -4,7 +4,7 @@ import pygame as pyg
 s = sc.Serial('COM2', 9600, timeout=0)
 pyg.init()
 
-window = (1200, 600)
+window = (1280, 720)
 
 screen = pyg.display.set_mode(window)
 
@@ -13,11 +13,9 @@ while True:
         if event.type == pyg.QUIT:
             sys.exit()
         if event.type == pyg.MOUSEBUTTONDOWN:
-            cords = pyg.mouse.get_pos()
-            cords = str(cords)
-            print(cords)
-            s.write(cords.encode())
-            # print(cords)
-
-# while True:
-#
+            x, y = pyg.mouse.get_pos()
+            x = x - 640
+            y = (y - 360)*-1
+            pos = (x, y)
+            print(pos)
+            #s.write(cords.encode())
